f3a12e9a9d8b74f5efd1a76df78d646f77d28a7b
Vinoth Sabanadesan <vinoth15@gmail.com>
2020-05-30 08:58:25 -0400

    cmd project added

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
+using Plossum.CommandLine;
+using Utility.Cmd.CommandLine;
+
 namespace Hyt.Yuk
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Options options = new Options();
+            CommandLineParser parser = new CommandLineParser(options);
+            parser.Parse();
+            Console.WriteLine(parser.UsageInfo.GetHeaderAsString(78));
+
+            if (options.Help)
+            {
+                Console.WriteLine(parser.UsageInfo.GetOptionsAsString(78));
+                return 0;
+            }
+            else if (parser.HasErrors)
+            {
+                Console.WriteLine(parser.UsageInfo.GetErrorsAsString(78));
+                return -1;
+            }
+            Console.WriteLine("Hello {0}!", options.Name);
+            return 0;
         }
     }
 }
