6aa789bf776b15e1bc9bac8014418076d59c672e
Vinoth Sabanadesan <vinoth15@gmail.com>
2020-06-06 01:40:07 -0400

    reorg proj console

---
+++ Hyt.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hyt.Console
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            System.Console.WriteLine("Hello {0}!", "test");
+            return 0;
+        }
+    }
+}
